b3107c6b8d4f5d44b413085e3791abb1791101d3
Peter Bruch <p.bruch@viamedici.de>
2019-05-29 19:00:42 +0200

    added example

--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using AutoProperties;
 
 namespace Tests
 {
@@ -11,10 +13,10 @@ namespace Tests
     {
         
     }
-    public class GenericBaseSome<TProperty>
+    public class GenericBaseSome<TProperty>:ChangeTrackable
     {
         private static PropertyInfo k__PropertyInfo;
-        private static TProperty k__BackingField;
+        private  TProperty k__BackingField;
 
        
         
@@ -36,14 +38,70 @@ namespace Tests
             }
         }
         
-        protected void SetValue<T>(string name, Type propertyType, PropertyInfo propertyInfo, object newValue, T genericNewValue,ref T refToBackingField)
-        {
+    }
+}
+public class SomeClassWithoutGenerics : ChangeTrackable
+{
+    private static PropertyInfo v__PropertyInfo;
+    private  int v__BackingField;
+    
+    private static PropertyInfo r__PropertyInfo;
+    private  string r__BackingField;
+    
+    private static PropertyInfo a__PropertyInfo;
+    private  string[] a__BackingField;
 
-            refToBackingField = genericNewValue;
-        }
-        protected T GetValue<T>(string name, Type propertyType, PropertyInfo propertyInfo, object fieldValue, T genericFieldValue, ref T refToBackingField)
+    public int ValueProperty
+    {
+        get
         {
-            return genericFieldValue;
+            return base.GetValue<int>("ValueProperty", typeof(int), v__PropertyInfo, v__BackingField, v__BackingField, ref v__BackingField);
         }
+        set
+        {
+            base.SetValue<int>("ValueProperty", typeof(int), v__PropertyInfo, value, value, ref v__BackingField);
+        }
+    }
+
+    public string ReferenceProperty
+    {
+        get
+        {
+            return base.GetValue<string>("ReferenceProperty", typeof(string), r__PropertyInfo, r__BackingField, r__BackingField, ref r__BackingField);
+        }
+        set
+        {
+            base.SetValue<string>("ReferenceProperty", typeof(string), r__PropertyInfo, (object)value, value, ref r__BackingField);
+        }
+    }
+
+    public string[] ArrayProperty
+    {
+        get
+        {
+            return base.GetValue<string[]>("ArrayProperty", typeof(string[]), a__PropertyInfo, a__BackingField, a__BackingField, ref a__BackingField);
+        }
+        set
+        {
+            base.SetValue<string[]>("ArrayProperty", typeof(string[]), a__PropertyInfo, (object)value, value, ref a__BackingField);
+        }
+    }
+}
+
+
+public abstract class ChangeTrackable
+{
+    public virtual HashSet<string> ChangedProperties { get; } = new HashSet<string>();
+
+    protected void SetValue<T>(string name, Type propertyType, PropertyInfo propertyInfo, object newValue, T genericNewValue,
+        ref T refToBackingField)
+    {
+        refToBackingField = genericNewValue;
+        ChangedProperties?.Add(name);
+    }
+
+    protected T GetValue<T>(string name, Type propertyType, PropertyInfo propertyInfo, object fieldValue, T genericFieldValue, ref T refToBackingField)
+    {
+        return genericFieldValue;
     }
 }
